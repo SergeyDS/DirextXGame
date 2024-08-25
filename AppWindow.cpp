20808d96084732fd3d108b59fe9bd732da7b7569
Sergey <sergey_1c@mail.ru>
2024-08-25 19:13:33 +0700

    Добавьте файлы проекта.

---
+++ AppWindow.cpp
@@ -0,0 +1,29 @@
+#include "AppWindow.h"
+
+
+
+AppWindow::AppWindow()
+{
+}
+
+
+AppWindow::~AppWindow()
+{
+}
+
+void AppWindow::onCreate()
+{
+	Window::onCreate();
+
+}
+
+void AppWindow::onUpdate()
+{
+	Window::onUpdate();
+
+}
+
+void AppWindow::onDestroy()
+{
+	Window::onDestroy();
+}
